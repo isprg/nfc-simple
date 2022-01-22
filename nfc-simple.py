d660f80eed84100ff093206e7ca4fbe619672cb4
Hiroshi Igaki <hirocell+gh@gmail.com>
2022-01-22 17:52:19 +0900

    Update nfc-simple.py
    文字ベースでぐるりパワーゲット

--- nfc-simple.py
+++ nfc-simple.py
@@ -2,15 +2,36 @@ import PySimpleGUI as sg
 import nfc
 import time
 import binascii
+import random
+import datetime
 
 class Gururi():
     def __init__(self):
         sg.theme('DarkBrown2')
         self.layout =[[sg.Text('ぐるりパワースポット')],
-                 [sg.Text('NFCID'), sg.Text(key='-IDm-', size=(40,1))],
-                 [sg.Text('色'), sg.Text(key='-color-', size=(40,1))]]
-        self.window = sg.Window('NFCタグリーダー', self.layout, size=(300, 120))
+                 [sg.Text('パワースポットの色'), sg.Text(key='-spotcolor-', size=(40,1))],
+                 [sg.Text('ハムスタンNFCID'), sg.Text(key='-IDm-', size=(40,1))],
+                 [sg.Text('ハムスタンの色'), sg.Text(key='-color-', size=(40,1))],
+                 [sg.Text(key='-msg-', size=(40,1))]]
+        self.window = sg.Window('NFCタグリーダー', self.layout, size=(300, 150))
+        self.spcolor = 0 # spot color
         #print(dir(self.window))
+    def compare_color(self):
+        # spot colorとnfccolorを比較する
+                
+        if self.idmcolor == 0:
+            print("赤です")
+            cr.window['-color-'].update("赤")
+        elif self.idmcolor == 1:
+            print("青です")
+            cr.window['-color-'].update("青")
+        elif self.idmcolor == 2:
+            cr.window['-color-'].update("緑")
+
+        if self.spcolor == self.idmcolor:
+            cr.window['-msg-'].update("ぐるりパワーげっと！")
+        else:
+            cr.window['-msg-'].update("")
         
     def check_ic(self):
         #print('Waiting Now')
@@ -22,12 +43,14 @@ class Gururi():
         if not target is None:
             tag = nfc.tag.activate(clf, target)
             #IDmのみ取得して表示
-            # たまに AttributeError: 'NoneType' object has no attribute '_nfcid' が発生する
             try:
-                self.idm = binascii.hexlify(tag._nfcid)
+                self.idm = int(binascii.hexlify(tag._nfcid).decode(),16) # bytesをdecodeして文字列にする->10進整数に変換
+                self.idmcolor = self.idm % 3
                 #print("IDm : " + str(self.idm))
                 self.window['-IDm-'].update(str(self.idm))
-            except AttributeError:
+                self.compare_color()
+                
+            except AttributeError:# たまに AttributeError: 'NoneType' object has no attribute '_nfcid' が発生するので対策
                 print("ID読み込み失敗")
                 
 
@@ -37,13 +60,26 @@ class Gururi():
         clf.close()
 if __name__ == '__main__':
     cr = Gururi()
+    store_status = 0 #スポットカラーを更新するかどうかを評価する変数
+
     while True:
         #time.sleep(1)
         event, values = cr.window.read(timeout=10,timeout_key='-timeout-')
         if event == sg.WIN_CLOSED:
             break
         elif event in '-timeout-':
-            cr.window['-color-'].update("赤")
+            dt = datetime.datetime.now()
+            status = dt.second // 5
+            if store_status != status: #5秒に1度更新されるstatusが変化したときにだけスポットカラーを変化させる
+                cr.spcolor = random.randint(0,2)
+                store_status = status
+            if cr.spcolor == 0:
+                strspcolor = "赤"
+            elif cr.spcolor == 1:
+                strspcolor = "青"
+            elif cr.spcolor == 2:
+                strspcolor = "緑"
+            cr.window['-spotcolor-'].update(strspcolor)
             cr.check_ic()
             
     cr.window.close()
